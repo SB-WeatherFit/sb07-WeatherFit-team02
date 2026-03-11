26ed82989df0e5e9da05a2e6c8ed4f2bbeb98e8a
OfficialHwempire <genius5375@gmail.com>
2026-03-11 15:16:27 +0900

    initial commit

---
+++ src/test/java/com/codeit/weatherfit/WeatherFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.weatherfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
