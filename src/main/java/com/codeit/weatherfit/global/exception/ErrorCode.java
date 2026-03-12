6b1d2300638379b88b93062534181d5c1f2c3fca
OfficialHwempire <genius5375@gmail.com>
2026-03-12 10:21:39 +0900

    feat:  weather fit exception변수 순서 변경

--- src/main/java/com/codeit/weatherfit/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/weatherfit/global/exception/ErrorCode.java
@@ -1,14 +1,17 @@
 package com.codeit.weatherfit.global.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
+
 
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"Internal Server Error","CM-001");
 
-    String code;
-    String message;
-    HttpStatus httpStatus;
+    final HttpStatus httpStatus;
+    final String message;
+    final String code;
+
     ErrorCode(HttpStatus httpStatus,String message,String code)
     {
         this.httpStatus=httpStatus;
---
+++ src/main/java/com/codeit/weatherfit/global/exception/WeatherFitException.java
@@ -0,0 +1,26 @@
+package com.codeit.weatherfit.global.exception;
+
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class WeatherFitException extends RuntimeException{
+
+    final ErrorCode errorCode;
+    final Map<String,Object> details;
+    public WeatherFitException(ErrorCode errorCode, Map<String,Object> details) {
+        details.put("timestamp", Instant.now());
+        this.errorCode = errorCode;
+        this.details = details;
+    }
+
+    public WeatherFitException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        Map<String, Object> tmp = new HashMap<>();
+        tmp.put("timestamp", Instant.now());
+        this.details = tmp;
+    }
+}
