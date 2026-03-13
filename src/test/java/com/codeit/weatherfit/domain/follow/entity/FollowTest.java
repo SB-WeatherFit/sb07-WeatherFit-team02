d3f8ea710975fc1387d680e665f7f977388b49a1
ian <dldks1290@gmail.com>
2026-03-13 11:35:31 +0900

    feat: 팔로우 서비스 틀 작성

---
+++ src/test/java/com/codeit/weatherfit/domain/follow/entity/FollowTest.java
@@ -0,0 +1,11 @@
+package com.codeit.weatherfit.domain.follow.entity;
+
+import org.junit.jupiter.api.Test;
+
+
+class FollowTest {
+    @Test
+    void create() {
+        //todo: user create 생기면
+    }
+}
