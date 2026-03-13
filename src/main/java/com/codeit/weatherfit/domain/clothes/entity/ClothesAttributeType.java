7970371d12741ac6cc19a6eb753332c62359a26b
OfficialHwempire <genius5375@gmail.com>
2026-03-13 14:30:55 +0900

    feat: cloth entity 변경

--- src/main/java/com/codeit/weatherfit/domain/clothes/entity/ClothesAttributeType.java
+++ src/main/java/com/codeit/weatherfit/domain/clothes/entity/ClothesAttributeType.java
@@ -2,23 +2,17 @@ package com.codeit.weatherfit.domain.clothes.entity;
 
 import com.codeit.weatherfit.domain.base.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name = "clothing_attributes")
+@Table(name = "clothes_attribute_types")
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ClothingAttribute extends BaseEntity {
+public class ClothesAttributeType extends BaseEntity {
 
-    @ManyToOne
-    private Clothes clothes;
+    private String name;
 
-    @ManyToOne
-    private ClothingAttributeType clothingAttributeType;
-
-    private String value;
 }
