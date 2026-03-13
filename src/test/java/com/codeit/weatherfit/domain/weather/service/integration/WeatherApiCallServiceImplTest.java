42fd9ef99c2324b7b3350576562307af8a9c6d94
OfficialHwempire <genius5375@gmail.com>
2026-03-13 18:01:38 +0900

    feat: 외부 api 테스트는 git ci에서 배제하는 코드 구현

--- src/test/java/com/codeit/weatherfit/domain/weather/service/integration/WeatherApiCallServiceImplTest.java
+++ src/test/java/com/codeit/weatherfit/domain/weather/service/integration/WeatherApiCallServiceImplTest.java
@@ -5,7 +5,9 @@ import com.codeit.weatherfit.domain.weather.dto.response.WeatherApi.WeatherApiRe
 import com.codeit.weatherfit.domain.weather.service.WeatherApiCallService;
 import com.codeit.weatherfit.domain.weather.util.TestFixture;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +23,9 @@ class WeatherApiCallServiceImplTest {
 
     @Test
     @DisplayName("[정상 케이스] - 날씨 api 호출 ")
+    @Tag("external")
     void testWeatherCall() {
+
         //given
         LocationResponse locationResponse = testFixture.locationFactory();
 
