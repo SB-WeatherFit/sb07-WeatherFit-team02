88cfa3338c5dce42c6bab4d53763f5f66c798acd
hyo37009 <hyo37009@gmail.com>
2026-03-16 15:25:21 +0900

    fix: @RequestBody 어노테이션 누락 수정
    - Feed 생성 API의 요청 본문을 매핑하기 위해 @RequestBody 어노테이션 추가
    - 요청 데이터가 제대로 매핑되지 않는 문제를 해결하기 위함

--- src/main/java/com/codeit/weatherfit/domain/feed/controller/FeedController.java
+++ src/main/java/com/codeit/weatherfit/domain/feed/controller/FeedController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class FeedController {
     private final FeedService feedService;
 
     @PostMapping
-    public ResponseEntity<FeedDto> createFeed(FeedCreateRequestDto request){
+    public ResponseEntity<FeedDto> createFeed(@RequestBody FeedCreateRequestDto request){
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 feedService.create(request)
         );
