393fe2b35c878bee1685d91edb6c864ca34aded9
hyo37009 <hyo37009@gmail.com>
2026-03-16 13:29:18 +0900

    refactor: Ootd 엔티티를 FeedClothes로 대체 및 관련 변경 적용
    - Ootd 및 OotdClothes 엔티티를 삭제하고 FeedClothes 엔티티로 통합
    - FeedClothes 생성 로직을 위한 create 정적 메서드 추가
    - schema.sql에서 'ootds' 테이블을 'feed_clothes'로 이름 변경 및 외래 키 이름 수정
    - 코드 및 테이블 네이밍 일관성을 확보하고 OOTD 관련 설계를 단순화하기 위함

---
+++ src/main/java/com/codeit/weatherfit/domain/feed/entity/FeedClothes.java
@@ -0,0 +1,30 @@
+package com.codeit.weatherfit.domain.feed.entity;
+
+import com.codeit.weatherfit.domain.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "feed_clothes")
+public class FeedClothes extends BaseEntity { // FeedCoordinate
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String imageUrl;
+
+    public static FeedClothes create(Feed feed, String name, String imageUrl) {
+        FeedClothes coordinate = new FeedClothes();
+        coordinate.feed = feed;
+        coordinate.name = name;
+        coordinate.imageUrl = imageUrl;
+        return coordinate;
+    }
+}
