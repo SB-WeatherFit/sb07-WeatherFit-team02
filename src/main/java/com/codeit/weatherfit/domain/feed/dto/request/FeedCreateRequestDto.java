75d9bbe510ab44b28f1558ec2b430edad2dd5daa
hyo37009 <hyo37009@gmail.com>
2026-03-16 09:46:46 +0900

    refactor: FeedCreateRequestDto 필드 타입 수정
    - clothedIds 필드의 타입을 List<Clothes>에서 List<UUID>로 변경
    - 엔티티 의존성을 제거하여 DTO의 독립성과 재사용성을 향상

--- src/main/java/com/codeit/weatherfit/domain/feed/dto/request/FeedCreateRequestDto.java
+++ src/main/java/com/codeit/weatherfit/domain/feed/dto/request/FeedCreateRequestDto.java
@@ -1,14 +1,12 @@
 package com.codeit.weatherfit.domain.feed.dto.request;
 
-import com.codeit.weatherfit.domain.clothes.entity.Clothes;
-
 import java.util.List;
 import java.util.UUID;
 
 public record FeedCreateRequestDto(
         UUID userId,
         UUID weatherId,
-        List<Clothes> clothedIds,
+        List<UUID> clothedIds,
         String content
 ) {
 }
