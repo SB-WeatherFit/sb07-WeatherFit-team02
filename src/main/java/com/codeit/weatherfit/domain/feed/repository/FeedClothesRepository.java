4812147d37c9374f537258f50bf9a9d727e76ade
hyo37009 <hyo37009@gmail.com>
2026-03-16 17:18:44 +0900

    feat: FeedClothes 관련 조회 메서드 추가
    - FeedClothesRepository에 Feed 객체를 기반으로 조회하는 findAllByFeed 메서드 추가
    - 특정 Feed와 연결된 FeedClothes 목록을 효율적으로 조회하기 위함

--- src/main/java/com/codeit/weatherfit/domain/feed/repository/FeedClothesRepository.java
+++ src/main/java/com/codeit/weatherfit/domain/feed/repository/FeedClothesRepository.java
@@ -1,9 +1,12 @@
 package com.codeit.weatherfit.domain.feed.repository;
 
+import com.codeit.weatherfit.domain.feed.entity.Feed;
 import com.codeit.weatherfit.domain.feed.entity.FeedClothes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface FeedClothesRepository extends JpaRepository<FeedClothes, UUID> {
+    List<FeedClothes> findAllByFeed(Feed feed);
 }
