c83f91fec51e85deb9cd403a66bbf929b4a5c483
ian <dldks1290@gmail.com>
2026-03-16 11:03:17 +0900

    fix: MessageEventListener 예외 로그 기능 추가

--- src/main/java/com/codeit/weatherfit/domain/message/service/event/MessageEventListener.java
+++ src/main/java/com/codeit/weatherfit/domain/message/service/event/MessageEventListener.java
@@ -1,23 +1,30 @@
 package com.codeit.weatherfit.domain.message.service.event;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MessageEventListener {
 
     private final SimpMessagingTemplate messagingTemplate;
 
-//    @Aysnc
+    @Async("messageTaskExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleMessage(MessageCreatedEvent event) {
-        messagingTemplate.convertAndSend(
-                "/sub/direct-messages_" + event.messageKey(),
-                event.content()
-        );
+        try {
+            messagingTemplate.convertAndSend(
+                    "/sub/direct-messages_" + event.messageKey(),
+                    event.content()
+            );
+        } catch (Exception e) {
+            log.error("메시지 전송 실패: messageKey={}, error={}", event.messageKey(), e.getMessage());
+        }
     }
 }
