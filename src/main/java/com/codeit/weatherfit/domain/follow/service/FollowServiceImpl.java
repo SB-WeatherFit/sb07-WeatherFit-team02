e27783bc1ac0fd49a275337271f6758812968312
ian <dldks1290@gmail.com>
2026-03-17 09:46:21 +0900

    feat: follow service와 테스트 작성

--- src/main/java/com/codeit/weatherfit/domain/follow/service/FollowServiceImpl.java
+++ src/main/java/com/codeit/weatherfit/domain/follow/service/FollowServiceImpl.java
@@ -1,45 +1,131 @@
 package com.codeit.weatherfit.domain.follow.service;
 
 import com.codeit.weatherfit.domain.follow.dto.request.FollowCreateRequest;
-import com.codeit.weatherfit.domain.follow.dto.request.FolloweeSearchCondition;
 import com.codeit.weatherfit.domain.follow.dto.request.FollowerSearchCondition;
+import com.codeit.weatherfit.domain.follow.dto.request.FolloweeSearchCondition;
 import com.codeit.weatherfit.domain.follow.dto.response.FollowDto;
 import com.codeit.weatherfit.domain.follow.dto.response.FollowListResponse;
 import com.codeit.weatherfit.domain.follow.dto.response.FollowSummaryDto;
+import com.codeit.weatherfit.domain.follow.entity.Follow;
+import com.codeit.weatherfit.domain.follow.entity.FollowCreateParam;
+import com.codeit.weatherfit.domain.follow.exception.AlreadyFollowException;
+import com.codeit.weatherfit.domain.follow.exception.NotExistFollowException;
 import com.codeit.weatherfit.domain.follow.repository.FollowRepository;
+import com.codeit.weatherfit.domain.user.entity.User;
+import com.codeit.weatherfit.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FollowServiceImpl implements FollowService{
 
     private final FollowRepository followRepository;
+//    private final ProfileRepository profileRepository; // todo: profileRepository 추가되면
+    private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public FollowDto follow(FollowCreateRequest createRequest) {
-        return null;
+         if (checkFollowExist(createRequest.followeeId(), createRequest.followerId())){
+             throw new AlreadyFollowException();
+         }
+
+        User followee = userRepository.findById(createRequest.followeeId()).orElseThrow();
+        User follower = userRepository.findById(createRequest.followerId()).orElseThrow();
+
+
+        Follow follow = Follow.create(new FollowCreateParam(followee, follower));
+        Follow save = followRepository.save(follow);
+
+        return FollowDto.create(save.getId(), null, null); // todo: profileRepository 추가 시 수정
     }
 
     @Override
-    public FollowSummaryDto getFollowSummary(UUID userId) {
-        return null;
+    public FollowSummaryDto getFollowSummary(UUID userId, UUID myId) {
+        // todo: userId를 통해 존재하는 유저인지 검증 -> existsById 추가
+        long followerCount = followRepository.getFollowerCount(userId);
+        long followingCount = followRepository.getFollowingCount(userId);
+        boolean followedByMe = false;
+        UUID followId = null;
+
+        Optional<Follow> optionalFollow = followRepository.findByFolloweeIdAndFollowerId(userId, myId);
+        if(optionalFollow.isPresent()){
+            followedByMe = true;
+            followId =  optionalFollow.get().getId();
+        }
+
+        boolean followingMe = checkFollowExist(myId, userId);
+        return FollowSummaryDto.create(userId, followerCount, followingCount, followedByMe, followId, followingMe);
     }
 
-    @Override
-    public FollowListResponse getFollowings(FollowerSearchCondition condition) {
-        return null;
+    private boolean checkFollowExist(UUID followeeId, UUID followerId) {
+        return followRepository.existsByFolloweeIdAndFollowerId(followeeId, followerId);
     }
 
     @Override
     public FollowListResponse getFollowees(FolloweeSearchCondition condition) {
-        return null;
+        List<Follow> follows = followRepository.searchFollowees(condition);
+        long totalCount = followRepository.countByFolloweeId(condition.followeeId());
+
+        return cursorSupport(follows, condition.limit(), totalCount);
     }
 
     @Override
-    public void unFollow(UUID followId) {
+    public FollowListResponse getFollowers(FollowerSearchCondition condition) {
+        List<Follow> follows = followRepository.searchFollowers(condition);
+        long totalCount = followRepository.countByFollowerId(condition.followerId());
 
+
+        return cursorSupport(follows, condition.limit(), totalCount);
+    }
+
+    private FollowListResponse cursorSupport(List<Follow> follows, long limit, long totalCount) {
+        Instant nextCursor = null;
+        UUID nextIdAfter ;
+        boolean hasNext = false;
+
+        if(follows.size() >limit){
+            follows.removeLast();
+            hasNext = true;
+            nextCursor = follows.getLast().getCreatedAt();
+        }
+
+        nextIdAfter = follows.getLast().getFollowee().getId();
+
+        List<FollowDto> data = follows.stream()
+                .map(follow ->  FollowDto.create(follow.getId(), null, null))
+                .toList();
+
+        // todo: ProfileRepository 쪽에서 List<Profile> 로 데이터 조회
+//        public List<Profile> findAllByUserIds(List<UUID> userIds) {
+//            return queryFactory
+//                    .selectFrom(profile)
+//                    .where(profile.user.id.in(userIds))
+//                    .fetch();
+//        }
+//        Map<UUID, Profile> profileMap = profileRepository.findAllByUserIdIn(userIds).stream()
+//            .collect(Collectors.toMap(
+//                p -> p.getUser().getId(), // Key: 유저 ID
+//                p -> p                    // Value: Profile 객체 자신
+//            ));
+
+
+        return new FollowListResponse(data, nextCursor, nextIdAfter, hasNext, totalCount);
+    }
+
+    @Override
+    @Transactional
+    public void unFollow(UUID followId) {
+        Follow follow = followRepository.findById(followId).orElseThrow(NotExistFollowException::new);
+
+        followRepository.delete(follow);
     }
 }
--- src/test/java/com/codeit/weatherfit/domain/follow/service/FollowServiceTest.java
+++ src/test/java/com/codeit/weatherfit/domain/follow/service/FollowServiceTest.java
@@ -1,7 +1,100 @@
 package com.codeit.weatherfit.domain.follow.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import com.codeit.weatherfit.domain.follow.dto.request.FollowCreateRequest;
+import com.codeit.weatherfit.domain.follow.dto.response.FollowDto;
+import com.codeit.weatherfit.domain.follow.dto.response.FollowSummaryDto;
+import com.codeit.weatherfit.domain.follow.entity.Follow;
+import com.codeit.weatherfit.domain.follow.entity.FollowCreateParam;
+import com.codeit.weatherfit.domain.follow.repository.FollowRepository;
+import com.codeit.weatherfit.domain.user.entity.User;
+import com.codeit.weatherfit.domain.user.entity.UserRole;
+import com.codeit.weatherfit.domain.user.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 class FollowServiceTest {
 
+    @Autowired
+    FollowService followService;
+    @Autowired
+    FollowRepository followRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void follow() {
+        User user = User.create("test@gmail.com", "nickname", UserRole.USER, "password");
+        User user2 = User.create("test2@gmail.com", "nickname2", UserRole.USER, "password");
+        User saved = userRepository.save(user);
+        User saved2 = userRepository.save(user2);
+
+        FollowCreateRequest followCreateRequest = new FollowCreateRequest(saved.getId(), saved2.getId());
+
+        em.flush();
+        em.clear();
+
+        FollowDto followDto = followService.follow(followCreateRequest);
+
+        //profileRepository 후에...
+//        assertThat(followDto.follower().userId()).isEqualTo(saved2.getId());
+//        assertThat(followDto.followUser().userId()).isEqualTo(saved.getId());
+//        assertThat(followDto.follower().name()).isEqualTo(saved2.getName());
+//        assertThat(followDto.followUser().name()).isEqualTo(saved.getName());
+    }
+
+    @Test
+    void summary() {
+        User user = User.create("test@gmail.com", "nickname", UserRole.USER, "password");
+        User user2 = User.create("test2@gmail.com", "nickname2", UserRole.USER, "password");
+        User user3 = User.create("test3@gmail.com", "nickname2", UserRole.USER, "password");
+        User saved = userRepository.save(user);
+        User saved2 = userRepository.save(user2);
+        User saved3 = userRepository.save(user3);
+
+        FollowCreateRequest followCreateRequest = new FollowCreateRequest(saved.getId(), saved2.getId());
+        FollowCreateRequest followCreateRequest2 = new FollowCreateRequest(saved.getId(), saved3.getId());
+
+        FollowDto followDto = followService.follow(followCreateRequest);
+        FollowDto followDto2 = followService.follow(followCreateRequest2);
+
+        em.flush();
+        em.clear();
+
+        FollowSummaryDto followSummary = followService.getFollowSummary(saved.getId(), saved2.getId());
+
+        assertThat(followSummary.followeeId()).isEqualTo(saved.getId());
+        assertThat(followSummary.followedByMe()).isTrue();
+        assertThat(followSummary.followedByMeId()).isEqualTo(followDto.id());
+        assertThat(followSummary.followingMe()).isFalse();
+        assertThat(followSummary.followerCount()).isEqualTo(2);
+        assertThat(followSummary.followeeCount()).isEqualTo(0);
+    }
+
+    @Test
+    void unFollow() {
+        User user = User.create("test@gmail.com", "nickname", UserRole.USER, "password");
+        User user2 = User.create("test2@gmail.com", "nickname2", UserRole.USER, "password");
+        User saved = userRepository.save(user);
+        User saved2 = userRepository.save(user2);
+
+        Follow savedFollow = followRepository.save(Follow.create(new FollowCreateParam(saved, saved2)));
+
+        em.flush();
+        em.clear();
+
+        followService.unFollow(savedFollow.getId());
+
+        boolean result = followRepository.existsById(savedFollow.getId());
+        assertThat(result).isFalse();
+    }
 }
