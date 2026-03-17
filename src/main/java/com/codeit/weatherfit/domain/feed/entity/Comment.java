b9d2e841156cc5ee3fcf6bc87dcb2ed4855bcff0
hyo37009 <hyo37009@gmail.com>
2026-03-17 15:49:13 +0900

    refactor: Comment 엔티티에서 @EntityListeners 제거
    - Comment 엔티티에서 불필요한 @EntityListeners(AuditingEntityListener.class) 어노테이션 제거
    - BaseEntity 클래스에 이미 포함된 설정으로 인해 중복 코드 정리

--- src/main/java/com/codeit/weatherfit/domain/feed/entity/Comment.java
+++ src/main/java/com/codeit/weatherfit/domain/feed/entity/Comment.java
@@ -3,25 +3,23 @@ package com.codeit.weatherfit.domain.feed.entity;
 import com.codeit.weatherfit.domain.base.BaseEntity;
 import com.codeit.weatherfit.domain.user.entity.User;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "comments")
-@EntityListeners(AuditingEntityListener.class)
 public class Comment extends BaseEntity {
 
     @ManyToOne
     private User author;
+
     @ManyToOne
     private Feed feed;
-    private String content;
 
+    private String content;
 }
