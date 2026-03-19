de0e128f1b128098f43c2af0bf21a8ba780f13b7
hyo37009 <hyo37009@gmail.com>
2026-03-19 16:21:27 +0900

    feat: Message 커서 기반 조회 기능 추가
    - MessageRepositoryCustom 및 MessageRepositoryCustomImpl 구현하여 커서 기반 조회 로직 추가
    - QueryDSL을 활용해 정렬, 필터링, 조건부 검색 처리
    - MessageRepository에 MessageRepositoryCustom 확장 인터페이스 통합
    - createdAt과 id를 기준으로 커서 기반 페이징 처리 로직 추가

---
+++ src/main/java/com/codeit/weatherfit/domain/message/repository/MessageRepositoryCustomImpl.java
@@ -0,0 +1,35 @@
+package com.codeit.weatherfit.domain.message.repository;
+
+import com.codeit.weatherfit.domain.message.dto.request.MessageGetRequest;
+import com.codeit.weatherfit.domain.message.entity.Message;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import static com.codeit.weatherfit.domain.message.entity.QMessage.message;
+
+@RequiredArgsConstructor
+public class MessageRepositoryCustomImpl implements MessageRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Message> getByCursor(MessageGetRequest request) {
+        return queryFactory.selectFrom(message)
+                .where(cursorCondition(request.cursor(), request.idAfter()))
+                .orderBy(message.createdAt.desc())
+                .limit(request.limit())
+                .fetch();
+    }
+
+    private BooleanExpression cursorCondition(Instant cursor, UUID idAfter) {
+        if (cursor == null || idAfter == null)
+            return null;
+        return message.createdAt.lt(cursor)
+                .or(message.createdAt.eq(cursor).and(message.id.lt(idAfter)));
+    }
+}
