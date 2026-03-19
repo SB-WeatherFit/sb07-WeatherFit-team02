a75fd2423585b798d038a3f366e63dab9b0ce320
ian <dldks1290@gmail.com>
2026-03-19 09:06:07 +0900

    feat: notification service 추가

---
+++ src/main/java/com/codeit/weatherfit/domain/notification/service/NotificationEventListener.java
@@ -0,0 +1,49 @@
+package com.codeit.weatherfit.domain.notification.service;
+
+import com.codeit.weatherfit.domain.notification.dto.response.NotificationDto;
+import com.codeit.weatherfit.domain.notification.event.PersonalNotificationEvent;
+import com.codeit.weatherfit.domain.notification.event.SystemNotificationEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationEventListener {
+    private final NotificationService notificationService;
+    private final SseService sseService;
+
+    @Async("notificationTaskExecutor")
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleMessageCreated(PersonalNotificationEvent event) {
+        NotificationDto notificationDto = notificationService.send(
+                event.getReceiverId(),
+                event.getTitle(),
+                event.getContent(),
+                event.getNotificationLevel()
+        );
+
+        sseService.send(notificationDto);
+    }
+
+    @Async("notificationTaskExecutor")
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void broadcast(SystemNotificationEvent event) {
+
+        Set<UUID> setUserIds = sseService.findSetUserIds();
+
+        List<NotificationDto> notificationDtos = notificationService.broadcast(
+                event.getTitle(),
+                event.getContent(),
+                event.getNotificationLevel(),
+                setUserIds);
+
+        sseService.broadcast(notificationDtos);
+    }
+}
