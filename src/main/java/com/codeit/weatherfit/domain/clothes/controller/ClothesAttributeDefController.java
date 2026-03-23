ac744686a16362e82a5b6172b2abd02fe5485f11
OfficialHwempire <genius5375@gmail.com>
2026-03-23 14:40:50 +0900

    feat: ClothesAttributeDefController 작성

--- src/main/java/com/codeit/weatherfit/domain/clothes/controller/ClothesAttributeDefController.java
+++ src/main/java/com/codeit/weatherfit/domain/clothes/controller/ClothesAttributeDefController.java
@@ -1,6 +1,8 @@
 package com.codeit.weatherfit.domain.clothes.controller;
 
+import com.codeit.weatherfit.domain.clothes.dto.request.ClothesAttributeDefCreateRequest;
 import com.codeit.weatherfit.domain.clothes.dto.request.ClothesAttributeDefGetRequest;
+import com.codeit.weatherfit.domain.clothes.dto.request.ClothesAttributeDefUpdateRequest;
 import com.codeit.weatherfit.domain.clothes.dto.response.ClothesAttributeDefDto;
 import com.codeit.weatherfit.domain.clothes.service.AttributeDefService;
 import jakarta.validation.Valid;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,47 +22,37 @@ public class ClothesAttributeDefController {
     private final AttributeDefService attributeDefService;
 
     @PostMapping
-    public ResponseEntity<Void> create() {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<ClothesAttributeDefDto> create(
+            @RequestBody @Valid ClothesAttributeDefCreateRequest request
+    ) {
+
+        ClothesAttributeDefDto response = attributeDefService.createAttributeDef(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping
-    public List<ClothesAttributeDefDto> getAll(
-            @RequestParam(required = false) String sortBy,
-            @RequestParam(required = false) String sortDirection
-    ) {
-//        return List.of(
-//                new ClothesAttributeDefDto(
-//                        UUID.randomUUID(),
-//                        "색상",
-//                        List.of("검정", "흰색", "파랑"),
-//                        Instant.now()
-//                ),
-//                new ClothesAttributeDefDto(
-//                        UUID.randomUUID(),
-//                        "사이즈",
-//                        List.of("S", "M", "L"),
-//                        Instant.now()
-//                )
-//        );
-        return attributeDefService.getAll();
-    }
 
     @GetMapping
     public ResponseEntity<List<ClothesAttributeDefDto>> getAttributeDefs(
-           @ModelAttribute @Valid ClothesAttributeDefGetRequest request
+            @ModelAttribute @Valid ClothesAttributeDefGetRequest request
     ) {
         List<ClothesAttributeDefDto> response = attributeDefService.getAttributeDefs(request);
         return ResponseEntity.ok().body(response);
     }
 
     @PatchMapping("/{definitionId}")
-    public ResponseEntity<Void> update(@PathVariable Long definitionId) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ClothesAttributeDefDto> update(
+            @PathVariable UUID definitionId,
+            @RequestBody @Valid ClothesAttributeDefUpdateRequest request
+            ) {
+
+        ClothesAttributeDefDto response = attributeDefService.patchAttributeDef(definitionId, request);
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{definitionId}")
-    public ResponseEntity<Void> delete(@PathVariable Long definitionId) {
+    public ResponseEntity<Void> delete(@PathVariable UUID definitionId) {
+
+        attributeDefService.deleteAttributeDef(definitionId);
         return ResponseEntity.noContent().build();
     }
 }
