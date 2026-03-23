7270e188134808ca7ba77cc38f30488a99bf7135
TaeHUN <tehon79@gmail.com>
2026-03-23 15:22:47 +0900

    fix : MessageControllerTest 수정

--- src/test/java/com/codeit/weatherfit/domain/message/controller/MessageControllerTest.java
+++ src/test/java/com/codeit/weatherfit/domain/message/controller/MessageControllerTest.java
@@ -1,15 +1,13 @@
 package com.codeit.weatherfit.domain.message.controller;
 
-import com.codeit.weatherfit.domain.message.dto.request.MessageGetRequest;
 import com.codeit.weatherfit.domain.message.dto.response.MessageCursorResponse;
 import com.codeit.weatherfit.domain.message.service.MessageService;
-import com.codeit.weatherfit.global.config.SecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.mockito.Mockito;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import tools.jackson.databind.ObjectMapper;
 
 import java.util.UUID;
@@ -20,29 +18,36 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@WebMvcTest(MessageController.class)
-@Import(SecurityConfig.class)
 class MessageControllerTest {
 
-    @Autowired
-    MockMvcTester mvcTester;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @MockitoBean
     private MessageService messageService;
 
+    private MockMvcTester mvcTester;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        messageService = Mockito.mock(MessageService.class);
+
+        MessageController messageController = new MessageController(messageService);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(messageController)
+                .build();
+
+        mvcTester = MockMvcTester.create(mockMvc);
+    }
 
     @Test
     void getMessageTest() throws Exception {
         when(messageService.searchMessages(any(), any()))
                 .thenReturn(new MessageCursorResponse(null, null, null, false, 10));
 
-        assertThat(mvcTester.get().uri("/api/direct-messages")
-                .param("userId", String.valueOf(UUID.randomUUID())) // @ModelAttribute는 파라미터로!
-                .param("limit", "20")
-        )
+        assertThat(
+                mvcTester.get()
+                        .uri("/api/direct-messages")
+                        .param("userId", String.valueOf(UUID.randomUUID()))
+                        .param("limit", "20"))
                 .apply(print())
                 .hasStatusOk();
 
