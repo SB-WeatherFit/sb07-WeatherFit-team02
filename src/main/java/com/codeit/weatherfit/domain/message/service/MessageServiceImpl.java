c1967ab9d864d6beeb32541ab2ea6f331f7c7704
ian <dldks1290@gmail.com>
2026-03-24 14:01:52 +0900

    fix: dm 수정

--- src/main/java/com/codeit/weatherfit/domain/message/service/MessageServiceImpl.java
+++ src/main/java/com/codeit/weatherfit/domain/message/service/MessageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.codeit.weatherfit.domain.message.service;
 
+import com.codeit.weatherfit.domain.message.dto.DmDto;
 import com.codeit.weatherfit.domain.message.dto.request.MessageCreateRequest;
 import com.codeit.weatherfit.domain.message.dto.request.MessageGetRequest;
 import com.codeit.weatherfit.domain.message.dto.response.MessageCursorResponse;
@@ -44,10 +45,14 @@ public class MessageServiceImpl implements MessageService {
                 .orElseThrow(); // todo
 
         Message message = Message.create(sender, receiver, content);
-        messageRepository.save(message);
+        Message save = messageRepository.save(message);
+        Profile receiverProfile = profileRepository.findWithUser(save.getReceiver().getId()).orElseThrow();
+        Profile senderProfile = profileRepository.findWithUser(save.getSender().getId()).orElseThrow();
+        DmDto messageDto = DmDto.from(save, senderProfile, receiverProfile);
+
 
         String dmKey = generateDmKey(senderId, receiverId);
-        eventPublisher.publishEvent(new MessageCreatedEvent(dmKey, content));
+        eventPublisher.publishEvent(new MessageCreatedEvent(dmKey, messageDto));
     }
 
     @Override
