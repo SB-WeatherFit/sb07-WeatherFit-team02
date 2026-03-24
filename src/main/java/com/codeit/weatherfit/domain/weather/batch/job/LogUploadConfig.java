7c42b0abad550eba7b31c9dc00da6c9ebe496b25
OfficialHwempire <genius5375@gmail.com>
2026-03-24 13:45:50 +0900

    feat: 새벽 2시마다 전날 log.gz s3에 업로드 하는 기능 구현

---
+++ src/main/java/com/codeit/weatherfit/domain/weather/batch/job/LogUploadConfig.java
@@ -0,0 +1,36 @@
+package com.codeit.weatherfit.domain.weather.batch.job;
+
+import com.codeit.weatherfit.domain.weather.batch.JobStatus;
+import com.codeit.weatherfit.domain.weather.batch.tasklet.LogUploadTasklet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class LogUploadConfig {
+
+    private final LogUploadTasklet logUploadTasklet;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job weatherDeleteJob() {
+        return new JobBuilder(JobStatus.WEATHER_DELETE.getJobName(),jobRepository)
+                .start(logUploadStep())
+                .build();
+    }
+
+    @Bean
+    public Step logUploadStep() {
+        return new StepBuilder(jobRepository)
+                .tasklet(logUploadTasklet,transactionManager)
+                .build();
+    }
+}
