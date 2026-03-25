105922f9d4499ed7f457be2ce16f8153cc8da963
ian <dldks1290@gmail.com>
2026-03-25 15:25:58 +0900

    fix: 커서 테스트 시  createdAt 중복 문제 해결

--- src/test/java/com/codeit/weatherfit/domain/message/repository/MessageCustomRepositoryTest.java
+++ src/test/java/com/codeit/weatherfit/domain/message/repository/MessageCustomRepositoryTest.java
@@ -2,7 +2,6 @@ package com.codeit.weatherfit.domain.message.repository;
 
 import com.codeit.weatherfit.domain.message.dto.request.MessageGetRequest;
 import com.codeit.weatherfit.domain.message.entity.Message;
-import com.codeit.weatherfit.domain.message.entity.UserFixture;
 import com.codeit.weatherfit.domain.user.entity.User;
 import com.codeit.weatherfit.domain.user.repository.UserRepository;
 import com.codeit.weatherfit.global.config.JpaAuditingConfig;
@@ -12,13 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
-import java.util.UUID;
 
-import static com.codeit.weatherfit.domain.message.entity.UserFixture.*;
+import static com.codeit.weatherfit.domain.message.entity.UserFixture.createUser;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @Import({QueryDslConfig.class, JpaAuditingConfig.class})
@@ -30,6 +30,8 @@ class MessageCustomRepositoryTest {
     UserRepository userRepository;
     @Autowired
     EntityManager em;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     void searchMessages() {
@@ -40,9 +42,14 @@ class MessageCustomRepositoryTest {
 
         for (int i = 0; i < 50; i++) {
             Message message;
-            if (i%2==0) message= Message.create(user, user2, "content"+i);
-            else  message = Message.create(user2, user, "content"+i);
-            messageRepository.save(message);
+            if (i % 2 == 0) message = Message.create(user, user2, "content" + i);
+            else message = Message.create(user2, user, "content" + i);
+            Message save = messageRepository.save(message);
+            jdbcTemplate.update(
+                    "UPDATE messages SET created_at = ? WHERE id = ?",
+                    Timestamp.from(Instant.now().minusSeconds(i * 60)),
+                    save.getId()
+            );
         }
 
         em.flush();
