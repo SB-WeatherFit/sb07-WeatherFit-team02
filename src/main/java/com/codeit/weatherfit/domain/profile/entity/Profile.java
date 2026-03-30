13659270a4a2add49a60451e28547f565c28121b
hyo37009 <hyo37009@gmail.com>
2026-03-30 09:08:37 +0900

    잘못 수정된 부분 롤백

--- src/main/java/com/codeit/weatherfit/domain/profile/entity/Profile.java
+++ src/main/java/com/codeit/weatherfit/domain/profile/entity/Profile.java
@@ -40,7 +40,7 @@ public class Profile extends BaseEntity {
     @Embedded
     private Location location;
 
-    @Column(name = "temperature_sensitivity")
+    @Column(name = "temperature_sensitivity", nullable = false)
     private int temperatureSensitivity;
 
     @Column(name = "profile_image_key")
