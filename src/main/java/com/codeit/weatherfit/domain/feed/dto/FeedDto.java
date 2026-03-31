674d246a124dfa202395734c83385d2728287307
TaeHUN <tehon79@gmail.com>
2026-03-31 15:25:04 +0900

    fix : 피드 프로필 오류 해결

--- src/main/java/com/codeit/weatherfit/domain/feed/dto/FeedDto.java
+++ src/main/java/com/codeit/weatherfit/domain/feed/dto/FeedDto.java
@@ -1,14 +1,8 @@
 package com.codeit.weatherfit.domain.feed.dto;
 
 import com.codeit.weatherfit.domain.feed.entity.Feed;
-import com.codeit.weatherfit.domain.user.dto.response.UserDto;
-import com.codeit.weatherfit.domain.weather.dto.response.PrecipitaionResponse;
+import com.codeit.weatherfit.domain.user.dto.response.UserFeedDto;
 import com.codeit.weatherfit.domain.weather.dto.response.SimpleWeatherResponse;
-import com.codeit.weatherfit.domain.weather.dto.response.TemperatureResponse;
-import com.codeit.weatherfit.domain.weather.entity.Precipitation;
-import com.codeit.weatherfit.domain.weather.entity.SkyStatus;
-import com.codeit.weatherfit.domain.weather.entity.Temperature;
-import com.codeit.weatherfit.domain.weather.entity.Weather;
 
 import java.time.Instant;
 import java.util.List;
@@ -18,7 +12,7 @@ public record FeedDto(
         UUID id,
         Instant createdAt,
         Instant updatedAt,
-        UserDto author,
+        UserFeedDto author,
         SimpleWeatherResponse weather,
         List<FeedClothesDto> ootds,
         String content,
@@ -26,12 +20,18 @@ public record FeedDto(
         Long commentCount,
         boolean likedByMe
 ) {
-    public static FeedDto from(Feed feed, List<FeedClothesDto> ootds, Long likeCount, Long commentCount, boolean likedByMe){
+    public static FeedDto from(
+            Feed feed,
+            List<FeedClothesDto> ootds,
+            Long likeCount,
+            Long commentCount,
+            boolean likedByMe
+    ) {
         return new FeedDto(
                 feed.getId(),
                 feed.getCreatedAt(),
                 feed.getUpdatedAt(),
-                UserDto.from(feed.getAuthor()),
+                UserFeedDto.from(feed.getAuthor()),
                 SimpleWeatherResponse.from(feed.getWeather()),
                 ootds,
                 feed.getContent(),
@@ -40,5 +40,4 @@ public record FeedDto(
                 likedByMe
         );
     }
-
 }
