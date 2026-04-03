16a73f2682cf69c54753cee191884a6972dd8f1c
hyo37009 <hyo37009@gmail.com>
2026-04-03 18:39:43 +0900

    feat: S3 업로드 예외 처리 생성자 추가
    - S3UploadException에 메시지와 파일명을 함께 전달할 수 있는 생성자 추가
    - 업로드 실패 시 더 상세한 오류 정보를 제공하기 위함

--- src/main/java/com/codeit/weatherfit/global/s3/exception/S3UploadException.java
+++ src/main/java/com/codeit/weatherfit/global/s3/exception/S3UploadException.java
@@ -3,8 +3,14 @@ package com.codeit.weatherfit.global.s3.exception;
 import com.codeit.weatherfit.global.exception.ErrorCode;
 
 public class S3UploadException extends S3Exception {
-    public S3UploadException(String originalFilename) {
+    public S3UploadException(String filename) {
         super(ErrorCode.FILE_UPLOAD_FAILURE);
-        this.getDetails().put("originalFilename", originalFilename);
+        this.getDetails().put("filename", filename);
+    }
+
+    public S3UploadException(String fileName, String message) {
+        super(ErrorCode.FILE_UPLOAD_FAILURE);
+        this.getDetails().put("filename", fileName);
+        this.getDetails().put("message", message);
     }
 }
