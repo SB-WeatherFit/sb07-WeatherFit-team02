3006ba0de998a65d3ed302ffd5007c241e39d6f4
hyo37009 <hyo37009@gmail.com>
2026-04-03 16:38:23 +0900

    feat: S3KeyGenerator 키 생성 로직 개선
    - 파일명 앞에 "images/" 디렉토리 경로를 추가하도록 수정
    - 고유성을 위해 UUID를 활용한 키 생성 방식 도입
    - S3 파일 저장 시 디렉토리 구조를 명시하며 충돌 방지 및 관리 용이성 강화

--- src/main/java/com/codeit/weatherfit/global/s3/util/S3KeyGenerator.java
+++ src/main/java/com/codeit/weatherfit/global/s3/util/S3KeyGenerator.java
@@ -1,7 +1,9 @@
 package com.codeit.weatherfit.global.s3.util;
 
+import java.util.UUID;
+
 public class S3KeyGenerator {
     public static String generateKey(String originalFilename) {
-        return System.currentTimeMillis() + "_" + originalFilename;
+        return "images/" + UUID.randomUUID() + "_" + originalFilename;
     }
 }
