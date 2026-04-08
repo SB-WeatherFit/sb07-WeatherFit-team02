fc11d01984fe6e3af6b796b1c96fd4889d2cf171
KIM JIYE <hyo37009@gmail.com>
2026-04-08 13:52:57 +0900

    Update ClothesAttributeRepository.java

--- src/main/java/com/codeit/weatherfit/domain/clothes/repository/ClothesAttributeRepository.java
+++ src/main/java/com/codeit/weatherfit/domain/clothes/repository/ClothesAttributeRepository.java
@@ -25,7 +25,4 @@ public interface ClothesAttributeRepository extends JpaRepository <ClothesAttrib
     @Query("delete from ClothesAttribute ca where ca.option.clothesAttributeType.id = :defId")
     void deleteByAttributeType(UUID defId);
 
-    @Query(
-            "select ca.option.option from ClothesAttribute ca " + "where ca.clothes = :clothes")
-    List<String> getClothesOptions(@Param("clothes") Clothes clothes);
 }
