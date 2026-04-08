7467d55a077c413bbde74859d8822cc0d173d007
ian <dldks1290@gmail.com>
2026-04-08 17:00:49 +0900

    feat: 알람 전체 전송 로직 수정

--- src/main/java/com/codeit/weatherfit/domain/notification/service/redis/RedisBroadcastSubscriber.java
+++ src/main/java/com/codeit/weatherfit/domain/notification/service/redis/RedisBroadcastSubscriber.java
@@ -4,8 +4,10 @@ import com.codeit.weatherfit.domain.notification.dto.response.NotificationDto;
 import com.codeit.weatherfit.domain.notification.entity.Notification;
 import com.codeit.weatherfit.domain.notification.repository.NotificationRepository;
 import com.codeit.weatherfit.domain.notification.service.SseService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,15 +16,15 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class RedisBroadcastSubscriber {
 
     private final SseService sseService;
     private final NotificationRepository notificationRepository; // 또는 Repository 직접 조회
     private final ObjectMapper objectMapper;
 
-    public void onMessage(String json) {
-
-        UUID groupId = UUID.fromString(json);
+    public void onMessage(String json) throws JsonProcessingException {
+        BroadcastEvent broadcastEvent = objectMapper.readValue(json, BroadcastEvent.class);
 
         Set<UUID> localUserIds = sseService.findSetUserIds();
 
@@ -31,7 +33,7 @@ public class RedisBroadcastSubscriber {
         }
 
         List<Notification> myNotifications = notificationRepository
-                .findAllByGroupIdAndReceiverIdIn(groupId, localUserIds);
+                .findAllByGroupIdAndReceiverIdIn(broadcastEvent.groupId(), localUserIds);
 
         for (Notification notification : myNotifications) {
             sseService.send(NotificationDto.create(notification));
