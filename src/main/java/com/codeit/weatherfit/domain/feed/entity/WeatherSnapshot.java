e96cefb8dabd49f91d695766bf9510da569739c2
hyo37009 <hyo37009@gmail.com>
2026-04-08 10:05:02 +0900

    feat: WeatherSnapshot에 강수 타입 필드 추가
    - WeatherSnapshot에 PrecipitationType 필드 추가 및 Weather 데이터로부터 매핑
    - 날씨 데이터 스냅샷에 강수 타입 정보를 포함하여 상세 기록을 보장하기 위함

--- src/main/java/com/codeit/weatherfit/domain/feed/entity/WeatherSnapshot.java
+++ src/main/java/com/codeit/weatherfit/domain/feed/entity/WeatherSnapshot.java
@@ -1,16 +1,19 @@
 package com.codeit.weatherfit.domain.feed.entity;
 
+import com.codeit.weatherfit.domain.weather.entity.PrecipitationType;
 import com.codeit.weatherfit.domain.weather.entity.SkyStatus;
 import com.codeit.weatherfit.domain.weather.entity.Weather;
 
 public record WeatherSnapshot(
         SkyStatus skyStatus,
+        PrecipitationType type,
         TemperatureSnapshot temperature
 
 ) {
     public static WeatherSnapshot from(Weather weather) {
         return new WeatherSnapshot(
                 weather.getSkyStatus(),
+                weather.getType(),
                 new TemperatureSnapshot(weather.getTemperatureCurrent(),
                         weather.getMin(),
                         weather.getMax())
