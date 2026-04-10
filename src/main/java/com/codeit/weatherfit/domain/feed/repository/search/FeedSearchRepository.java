d1ff38697813a1b115247b357509a92e545fcbdb
hyo37009 <hyo37009@gmail.com>
2026-04-10 13:40:25 +0900

    feat: 피드 문서 및 검색 리포지토리 추가
    - FeedDocument 클래스 추가로 Elasticsearch에 인덱싱할 피드 문서 구조 정의
    - FeedSearchRepository 인터페이스 추가로 피드 검색 및 삭제 기능 구현 지원
    - 피드 검색 기능 강화 및 Elasticsearch와의 통합 작업을 위한 사전 준비 작업

---
+++ src/main/java/com/codeit/weatherfit/domain/feed/repository/search/FeedSearchRepository.java
@@ -0,0 +1,10 @@
+package com.codeit.weatherfit.domain.feed.repository.search;
+
+import com.codeit.weatherfit.domain.feed.entity.search.FeedDocument;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.UUID;
+
+public interface FeedSearchRepository extends ElasticsearchRepository<FeedDocument, String> {
+    void deleteByFeedId(UUID feedId);
+}
