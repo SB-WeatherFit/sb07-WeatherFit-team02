1d6c9f20b2d8c577f82e96b6ba839543673fec4f
hyo37009 <hyo37009@gmail.com>
2026-04-14 09:38:42 +0900

    refactor: 배치 쿼리 조회로 전환

--- src/main/java/com/codeit/weatherfit/domain/feed/repository/CommentRepository.java
+++ src/main/java/com/codeit/weatherfit/domain/feed/repository/CommentRepository.java
@@ -4,7 +4,9 @@ import com.codeit.weatherfit.domain.feed.entity.Comment;
 import com.codeit.weatherfit.domain.feed.entity.Feed;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID>, CommentRepositoryCustom {
@@ -12,4 +14,9 @@ public interface CommentRepository extends JpaRepository<Comment, UUID>, Comment
     Long countByFeed(Feed feed);
 
     void deleteByFeed(Feed feed);
+
+    @Query("select c.feed.id, count(c) from Comment c " +
+            "where c.feed.id in :feedIds " +
+            "group by c.feed.id")
+    List<Object[]> countByFeedIn(List<UUID> feedIds);
 }
